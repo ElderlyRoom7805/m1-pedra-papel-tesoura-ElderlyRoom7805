595b732efdac004f82998e2ce2859eac7e253707
ElderlyRoom7805 <jubiscleudogaymipray@gmail.com>
2024-06-06 15:37:06 -0300

    função pedra, papel e tesoura
    contém a função com de ganhar ou empatar de cada jogadores.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    //empate
+    if((player1 === "Pedra") && (player2 === "Pedra")) {
+        return "Empate!"
+    }
+    else if ((player1 === "Tesoura") && (player2 === "Tesoura")) {
+        return "Empate!"
+    }
+    else if ((player1 === "Papel") && (player2 === "Papel")) {
+        return "Empate!"
+    }
+
+    //jogador1 ganha
+    else if ((player1 === "Pedra") && (player2 === "Tesoura")) {
+        return "Jogador 1 venceu!"
+    }
+    else if ((player1 === "Tesoura") && (player2 === "Papel")) {
+        return "Jogador 1 venceu!"
+    }
+    else if ((player1 === "Papel") && (player2 === "Pedra")) {
+        return "Jogador 1 venceu!"
+    }
+    
+    //jogador2 ganha
+    else if ((player1 === "Pedra") && (player2 === "Papel")) {
+        return "Jogador 2 venceu!"
+    }
+    else if ((player1 === "Papel") && (player2 === "Tesoura")) {
+        return "Jogador 2 venceu!"
+    }
+    else if ((player1 === "Tesoura") && (player2 === "Pedra")) {
+        return "Jogador 2 venceu!"
+    }
+}
